c4a1541606808a9299ad47d62ff0cbeba5c3e625
Woosterwebdev <136399860+Woosterwebdev@users.noreply.github.com>
2024-04-01 09:01:07 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-/* see www.emojiterra.com for more possible image options */
+/* see www.emojiterra.com for more possible image options 
+
+    <div class="menu-item">
+        <div class="item-details">
+          <img class="item-img" src="images/beer.png" />
+          <div class="item-description">
+            <h3 class="item-name">Beer</h3>
+            <p class="ingredients">Grain, hops, yeast, water</p>
+            <p class="price">$12</p>
+          </div>
+        </div>
+        <img id="add-icon" src="images/add-icon.png" />
+    </div>
+*/
